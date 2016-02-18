24a43f01c5adf25745558d8d7c2724f990026b96
gsp26 <g.s.parihar@gmail.com>
2016-02-18 23:32:16 +0530

    String/StringStream

---
+++ C++/stringStream.cpp
@@ -0,0 +1,32 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+   // Complete this function
+    stringstream ss;
+    ss.str(str);
+    int k;
+    char ch;
+    vector<int> result;
+    while(ss>>k)
+    {
+        result.push_back(k);
+        ss>>ch;
+    }
+    return result;
+
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
+
